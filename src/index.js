e50a3d3e2bd0dd199de15a64bad3c471d9afa46f
Ulyana Bychkovskaya <ubychkovskaya20@gmail.com>
2019-10-02 21:46:36 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+  let string1 = first.split('').reverse().join('');
+  let string2 = second.split('').reverse().join('');
+  let multipliers = [];
+
+  for(let i=0; i<string1.length; i++) {
+    for(let j=0; j<string2.length; j++) {
+      let elem = string1[i]*string2[j];
+      (multipliers[i+j])? multipliers[i+j] += elem : multipliers[i+j] = elem;
+    }
+  }
+
+  for(let i=0; i<multipliers.length; i++) {
+    let remainder = multipliers[i]%10; 
+      let change = Math.floor(multipliers[i]/10);
+      if(change >= 1) {
+        multipliers[i] = remainder;
+        if(multipliers[i+1]) {
+          multipliers[i+1] += change;
+        }
+        else {
+          multipliers[i+1] = change;
+        }
+      }
+  }
+
+  return multipliers.reverse().join('');
+} 
